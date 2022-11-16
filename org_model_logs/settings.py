a900f74e49fbeb309e836387ae5772b946b7b1e6
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2022-11-16 11:32:01 +0800

    Added the common actions variables to the settings file.

---
+++ org_model_logs/settings.py
@@ -0,0 +1,7 @@
+ACTION_VIEW = "View {} {}"
+ACTION_LIST = "List {} {}"
+ACTION_CREATE = "Create {} {}"
+ACTION_UPDATE = "Update {} {}"
+ACTION_PARTIAL_UPDATE = "Partial Update {} {}"
+ACTION_DESTROY = "Destroy {} {}"
+ACTION_CANCEL = "Cancel {} {}"
